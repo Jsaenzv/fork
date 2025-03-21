e3be847e24d1446ace4fe3431915cb9a92a806e5
Justo Saenz Valiente <jsaenz@Justos-MacBook-Pro.local>
2025-03-21 14:44:15 -0300

    Corrijo errores en primes.c y consigo pasar tests

--- primes.c
+++ primes.c
@@ -4,70 +4,86 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define TERMINAR_PROGRAMA -1
+#define ULTIMO_PRIMO 0
+
 
 int main(int argc, char *argv[])
 {
-	printf("Hola mundo");
 	if (argc < 2) {
 		printf("Debes indicar un numero para calcular los primos menores a ese numero\n");
-		return 2;
+		exit(EXIT_FAILURE);
 	}
 	char* n_char = argv[1];
 	int n = atoi(n_char);
-	int numeros_primos[n - 1];
-	int j = 2;
 	int pipe_izquierdo[2];
 	int r = pipe(pipe_izquierdo);
-	for (int i = 0; i < n - 1; i++) {
-		numeros_primos[i] = j;
-		j++;
-	}
 	if (r < 0) {
 		printf("Error en pipe");
-		return 2;
+		exit(EXIT_FAILURE);
 	}
-	int primo_recibido;
+	ssize_t bytes_escritos = 0;
+	ssize_t bytes_leidos = 0;
+	
 	int pid = fork();
 	if (pid == 0) {
-		// Soy el hijo
+		int primo_recibido;
 		close (pipe_izquierdo[1]);
-		while (read(pipe_izquierdo[0], &primo_recibido, sizeof(primo_recibido)) > 0) {
-			printf("Primo: %i\n", primo_recibido);
+		while (((bytes_leidos = read(pipe_izquierdo[0], &primo_recibido, sizeof(primo_recibido)))) > 0) {
+			if (bytes_leidos == -1) {
+				printf("Error al leer del pipe");
+			}
+			printf("primo %i\n", primo_recibido);
 			int pipe_derecho[2];
 			int r = pipe(pipe_derecho);
 			if (r < 0) {
-				printf("error en fork");
-				return 2;
+				printf("error en pipe");
+				exit(EXIT_FAILURE);
 			}
-			int pid = fork();
+			pid = fork();
 			if (pid < 0) {
 				printf("Error en fork");
-				return 2;
+				exit(EXIT_FAILURE);
 			}
 			if (pid == 0) {
-				// soy el hijo
+				int pipe_cerrar = pipe_izquierdo[0];
 				pipe_izquierdo[0] = pipe_derecho[0];
-				close(pipe_derecho[1]);
-				close (pipe_izquierdo[0]);
+				close(pipe_cerrar);
+				close (pipe_derecho[1]);
 			}
 			else {
 				int i = primo_recibido;
-				while ((read(pipe_izquierdo[0], &primo_recibido, sizeof(primo_recibido))) > 0){
+				close(pipe_derecho[0]);
+				while (((bytes_leidos = read(pipe_izquierdo[0], &primo_recibido, sizeof(primo_recibido)))) > 0){
+					if (bytes_leidos == 0) {
+						printf("Error al leer del pipe");
+					}
 					if (primo_recibido % i != 0) {
-						write(pipe_derecho[1], &primo_recibido, sizeof(primo_recibido));
+						bytes_escritos = write(pipe_derecho[1], &primo_recibido, sizeof(primo_recibido));
+						if (bytes_escritos == -1) {
+							printf("Error al escribir en pipe");
+							exit(EXIT_FAILURE);
+						}
 					}
 				}
 				close (pipe_izquierdo[0]);
 				close (pipe_derecho[1]);
+				wait(NULL);
 			}
 		}
+		close (pipe_izquierdo[0]);
+		return 0;
 	}
 	else {
-		printf("Primo: %i\n", numeros_primos[0]);
+		printf("primo %i\n", 2);
 		close (pipe_izquierdo[0]);
-		for (int i = 0; i < n - 1; i++) {
-			if (numeros_primos[i] % 2 != 0) {
-				write(pipe_izquierdo[1], &numeros_primos[i], sizeof(numeros_primos[i]));
+		for (int i = 3; i <= n; i++) {
+			if (i % 2 != 0) {
+				bytes_escritos = write(pipe_izquierdo[1], &i, sizeof(i));
+				if (bytes_escritos == -1) {
+					printf("Error al escribir en pipe");
+					exit(EXIT_FAILURE);
+				}
 			}
 		}
 		close (pipe_izquierdo[1]);
@@ -75,18 +91,3 @@ int main(int argc, char *argv[])
 	}
 	return 0;
 }
-
-
-	// Pseudocódigo:
-// 	p := <leer valor de pipe izquierdo>
-
-// imprimir p // asumiendo que es primo
-
-// mientras <pipe izquierdo no cerrado>:
-//     n = <leer siguiente valor de pipe izquierdo>
-//     si n % p != 0:
-//         escribir <n> en el pipe derecho
-
-// (El único proceso que es distinto, es el primero, que tiene que simplemente generar la secuencia de números naturales de 2 a n. No tiene lado izquierdo.)
-
-// $ ./primes <n>
